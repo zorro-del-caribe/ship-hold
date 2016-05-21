d1b713f3c4b1f566b8e017eeeb8029dbdd7c7344
Lorenzofox3 <laurent34azerty@gmail.com>
2016-05-21 12:46:14 +0200

    save work

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 const connectionString = "postgres://docker:docker@192.168.99.100:5432/dev";
 const models = require('./lib/model');
-const qb = require('ship-hold-querybuilder')
+const qb = require('ship-hold-querybuilder');
 
-const model = models({table: 'users'}, connectionString);
+const model = models({
+  table: 'users', columns: {
+    id: 'integer',
+    age: 'integer',
+    name: 'string'
+  }
+}, connectionString);
 
-// const user4 = model
-//   .new({id: 4});
+const newUSer=model
+  .new({id:777,name:'blah',age:1})
+  .save()
+  .then(r=>console.log(r))
+  .catch(e=>console.log(e));
 
-
-setInterval(function () {
-  const user4 = model.new({id: 4})
-    .fetch('id', 'age', 'name')
-    .then(x=>x);
-}, 100);
-
-// const User = sf(model);
-// model
-//   .insert({
-//     id: '$id',
-//     age: '$age',
-//     name: '$age'
-//   })
-//   .returning('*')
-//   .run({id: 7, name: 'ed', age: 16})
-//   .then(rows => console.log(rows))
-//   .catch(err=>console.log(err));
-
-// model
-//   .select()
-//   // .where('name', '"users"."age"::text')
+// model.select()
 //   .run()
-//   .then(rows=>console.log(rows))
-//   .catch(err=>console.log(err));
+//   .then(res=> {
+//       console.log(res)
+//   })
+
+
+// const user666 = model.new({id: 666})
+//   .fetch()
+//   .then(result=>console.log(result));
+
+
+
+
