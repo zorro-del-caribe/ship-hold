e8fc4828839fde9f921c1d93d417be8b92adb622
Laurent RENARD <lorenzofox@Laurents-MacBook-Pro.local>
2019-09-04 11:25:51 -0400

    fix #12

--- dist/src/doc/introduction/snippets/init.js
+++ dist/src/doc/introduction/snippets/init.js
@@ -1,6 +1,6 @@
 const {shiphold} = require('ship-hold');
 const sh = shiphold({
-    hostname: '127.0.0.1',
+    host: '127.0.0.1',
     user: 'docker',
     password: 'docker',
     database: 'dev'
